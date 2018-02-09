078c1b53fc41c34366f1129326bc3239697dff1c
avenirs05 <avenirs05@gmail.com>
2018-02-09 23:21:00 +0200

    last

---
+++ index.php
@@ -0,0 +1,31 @@
+﻿<!doctype html>
+<html>
+  <head>
+    <title>Конвертер единиц концентрации газов</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+    <link href="style-calc.css" rel="stylesheet">
+  </head>
+
+  <body> 
+
+		<div id="calc-wrapper">
+			<h2>Калькулятор расчета стоимости москитной сетки</h2>
+			<form>	    
+				
+		  
+			</form>
+		</div>
+	
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- My Own Js-scripts -->
+	<script src="data.js"></script>	
+	<script src="js/main.js"></script>
+
+
+	</body>	
+	</html>
+
